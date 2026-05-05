2e050776a940c94562bef3d46b62b88376136e43
ford-HUB <dyfordbonghanoy@gmail.com>
2026-05-06 04:18:22 +0800

    fix(web): _base_ environment mismatch

--- web/src/api/_base_.js
+++ web/src/api/_base_.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const apiInstance = axios.create({
-    baseURL: import.meta.env.VITE_NODE === 'production' ? import.meta.env.VITE_SERVER_PRODUCTION : import.meta.env.VITE_SERVER_LOCAL,
+    baseURL: import.meta.env.VITE_ENV === 'production' ? import.meta.env.VITE_SERVER_PRODUCTION : import.meta.env.VITE_SERVER_LOCAL,
     withCredentials: true,
 })
