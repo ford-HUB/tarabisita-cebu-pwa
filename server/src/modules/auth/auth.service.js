9c6eb362aa50037b8687768c92ab32b5e4239fcd
ford-HUB <dyfordbonghanoy@gmail.com>
2026-05-06 19:16:07 +0800

    fix(server): nodemailer timeout

--- server/src/modules/auth/auth.service.js
+++ server/src/modules/auth/auth.service.js
@@ -2,26 +2,23 @@ import { transporter } from "../../configs/nodemailer.js";
 import { templateReader } from "../../shared/utils/templateReaderExtractor.js";
 
 export const sendMailer = async (to, subject, html) => {
-    try {
-        const message = await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to,
-            subject,
-            html
-        })
-
-        return console.log(message.response)
-    } catch (error) {
-        throw error
-    }
+    await new Promise((resolve, reject) => {
+        try {
+            const message = transporter.sendMail({
+                from: process.env.EMAIL_USER,
+                to,
+                subject,
+                html
+            })
+            resolve(message.response)
+            return console.log(message.response)
+        } catch (error) {
+            reject(error)
+        }
+    })
 }
 
-/**
- * @param {string} to
- * @param {string} subject
- * @param {string} html
- * @param {{ filename: string, content: Buffer, contentType?: string }[]} attachments
- */
+
 export const sendMailerWithAttachments = async (to, subject, html, attachments = []) => {
     const normalized = (attachments || [])
         .filter((a) => a && a.filename && a.content)
