b0e4198b966a28155d472fafa4b69579b3670882
ford-HUB <dyfordbonghanoy@gmail.com>
2026-05-06 21:09:01 +0800

    fix(server): smtp timeout

--- server/src/modules/auth/auth.service.js
+++ server/src/modules/auth/auth.service.js
@@ -1,31 +1,22 @@
-import { transporter } from "../../configs/nodemailer.js";
+import { sendBrevoEmail } from "../../configs/nodemailer.js";
 
 export const sendMailer = async (to, subject, html) => {
-    const message = await transporter.sendMail({
-        from: process.env.EMAIL_USER || process.env.BREVO_LOGIN,
+    const message = await sendBrevoEmail({
         to,
         subject,
         html
     })
+
     return message
 }
 
 
 export const sendMailerWithAttachments = async (to, subject, html, attachments = []) => {
-    const normalized = (attachments || [])
-        .filter((a) => a && a.filename && a.content)
-        .map((a) => ({
-            filename: a.filename,
-            content: a.content,
-            contentType: a.contentType || undefined
-        }))
-
-    const message = await transporter.sendMail({
-        from: process.env.EMAIL_USER || process.env.BREVO_LOGIN,
+    const message = await sendBrevoEmail({
         to,
         subject,
         html,
-        ...(normalized.length ? { attachments: normalized } : {})
+        attachments
     })
 
     return message
