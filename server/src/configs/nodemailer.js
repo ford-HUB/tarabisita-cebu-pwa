2931a34f8a17f0789b0d4b06c8cf46323876b527
ford-HUB <dyfordbonghanoy@gmail.com>
2026-05-06 20:39:50 +0800

    add(server): switch to port 587 test

--- server/src/configs/nodemailer.js
+++ server/src/configs/nodemailer.js
@@ -3,8 +3,8 @@ import 'dotenv/config'
 
 export const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.BREVO_LOGIN,
         pass: process.env.BREVO_APIKEY
