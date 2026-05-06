e73c2156ee939376612a9c12aa19ae195c546810
ford-HUB <dyfordbonghanoy@gmail.com>
2026-05-06 20:25:29 +0800

    fix(server): smtp connection duration and test tls 465

--- server/src/configs/nodemailer.js
+++ server/src/configs/nodemailer.js
@@ -3,10 +3,13 @@ import 'dotenv/config'
 
 export const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.BREVO_LOGIN,
         pass: process.env.BREVO_APIKEY
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
 })
